4160c9b14d68e683c42e4db2560553de74647efd
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-01-19 18:49:26 -0300

    feat: novas rotas com lazy loading para os componentes da página

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'instructions',
+    loadComponent: () =>
+      import('./pages/instructions/instructions.component').then(
+        (c) => c.InstructionsComponent
+      ),
+  },
+  {
+    path: 'categories',
+    loadComponent: () =>
+      import('./pages/categories/categories.component').then(
+        (c) => c.CategoriesComponent
+      ),
+  },
+  {
+    path: 'game',
+    loadComponent: () =>
+      import('./pages/game/game.component').then((c) => c.GameComponent),
+  },
+];
