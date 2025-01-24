b795bb130e4c917241a2cbb13dbf7e9f0b0ea0ac
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-01-23 21:20:04 -0300

    feat: página categories constuída

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,12 +1,32 @@
+import { LowerCasePipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgClass],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent {
+  categories: readonly string[] = [
+    'Animais',
+    'Cidades',
+    'Países',
+    'Nomes próprios',
+    'Frutas',
+    'Filmes',
+    'Cantores/Bandas',
+    'Esportes',
+    'Comidas',
+    'Objetos',
+    'Partes do corpo',
+    'Marcas',
+    'Profissões',
+    'Programas de TV',
+    'Personagens fictícios',
+  ];
 
+  categorySelected: string = this.categories[0];
 }
