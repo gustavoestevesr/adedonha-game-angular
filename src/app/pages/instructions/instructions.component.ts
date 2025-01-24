d750c03e652efc32fd912b7eb484975f6f6a68c7
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-01-23 21:21:08 -0300

    feat: página instructions constuída

--- src/app/pages/instructions/instructions.component.ts
+++ src/app/pages/instructions/instructions.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-instructions',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './instructions.component.html',
   styleUrl: './instructions.component.scss'
 })
